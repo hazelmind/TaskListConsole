8548a28b2db7b60840fa2bfc5475f44ebd5ff643
hzlmnd <52181893+hazelmind@users.noreply.github.com>
2019-11-22 14:36:45 -0500

    Pure C# .net core; task class, task list, add to list, and change status options

---
+++ TaskListConsole/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TaskListConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            List<Task> ListAll = new List<Task>
+                        {
+                            new Task{Count=i++, Name="Karey", Duedate="01/01/2020", Status="[ ] Incomplete", Description="Help someone reach their goal"},
+                            new Task{Count=i++,Name="Larry", Duedate="02/02/2020", Status="[ ] Incomplete", Description="Finish the program"},
+                            new Task{Count=i++,Name="Marry", Duedate="03/03/2020", Status="[X] Complete", Description="Finish CAD project"},
+
+                        };
+            //welcome prompt
+            Console.WriteLine("Hello! What would you like to do?");
+            //menu,
+            bool go = true;
+            while (go)
+            {
+
+                Console.WriteLine("[ 1 ] List Tasks");
+                Console.WriteLine("[ 2 ] Add Task");
+                Console.WriteLine("[ 3 ] Delete Task");
+                Console.WriteLine("[ 4 ] Mark Task Complete");
+                Console.WriteLine("[ 5 ] Quit");
+                int userPick = TryCatchInput("");
+                switch (userPick)
+                {
+                    case 1://PrintList
+                        Task.PrintList(ListAll);
+                        break;
+
+                    case 2:
+
+                        int taskNum = ListAll.Count;
+                        ListAll.Add(new Task { Count = taskNum, Name = Task.GetInputPrompt("Enter team members name: "), Duedate = Task.GetInputPrompt("Enter due date for task"), Status = "[ ] Incomplete", Description = Task.GetInputPrompt("Enter a description of the task: ") });
+                        break;
+
+                    case 3:
+
+                        int numDelete = TryCatchInput("What task number would you like to delete?");
+                        string askDelete = Task.GetInputPrompt("Are you sure you want to delete " + numDelete);
+
+                        string userSaid = askDelete.ToLower();
+                        if (userSaid == "yes" || userSaid == "y")
+                        {
+                            ListAll.RemoveAt(numDelete);
+                        }
+                        else
+                        {
+                        }
+                        break;
+
+                    case 4:
+
+                        int numberComp = TryCatchInput("Enter the task number you want to mark complete");
+                        int number = numberComp - 1;
+
+                        string input = Task.GetInputPrompt("Would you like to mark as complete? [Yes] [No]");
+                        Task.StatusIs(input, number, ListAll);
+                        break;
+
+                    case 5:
+                        //Quit
+                        go = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry, thats not an option.");
+                        go = true;
+                        break;
+
+                }
+            }
+        }
+
+        public static int TryCatchInput(string message)
+        {
+            string input = Task.GetInputPrompt(message);
+            try
+            {
+                int userInput = int.Parse(input);
+                return userInput;
+            }
+            catch (FormatException)
+            {
+                return TryCatchInput("That was not a number. Please enter a number.");
+            }
+            catch (OverflowException)
+            {
+                return TryCatchInput("That number was too large. Please try another option.");
+            }
+            catch (Exception e)
+            {
+                return TryCatchInput(e.GetType() + " Please enter a number");
+            }
+        }
+    }
+}
+//public DateTime DateReader()
+//{
+//    Console.WriteLine("Enter date :");
+//    string dateSaid = Console.ReadLine();
+//    string[] dateStrings = { dateSaid };
+//    foreach (string dateString in dateStrings)
+//    {
+//        DateTime convertedDate = DateTime.Parse(dateString);
+//        return convertedDate;
+//    };
+//}
---
+++ TaskListConsole/Task.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskListConsole
+{
+    class Task
+    {
+        //field
+
+        string name;
+        string duedate;
+        string status;
+        string description;
+        //properties
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public string Duedate
+        {
+            get
+            {
+                return duedate;
+            }
+            set
+            {
+                duedate = value;
+            }
+        }
+        public string Status
+        {
+            get
+            {
+                return status;
+            }
+            set
+            {
+                status = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public int Count
+        {
+            get; set;
+        }
+
+
+
+        //constructors
+
+        public Task()
+        {
+
+        }
+
+        public Task(string _name, string _duedate, string _status, string _description)
+        {
+            name = _name;
+            duedate = _duedate;
+            status = _status;
+            description = _description;
+        }
+        public void PrintInfo()
+        {
+            //
+            Console.WriteLine("_________________________________________________");
+            Console.WriteLine("Task Number " + (Count) + "]  Team member: " + Name + "");
+            Console.WriteLine("Task Description: " + Description);
+            Console.WriteLine("Due Date: " + Duedate + "   Status: " + Status);
+            Console.WriteLine("_________________________________________________");
+        }
+        public static string GetInputPrompt(string prompt)
+        {
+            Console.WriteLine(prompt);
+            string inputPrompted = Console.ReadLine();
+            return inputPrompted;
+        }
+
+        public static List<Task> PrintList(List<Task> list)
+        {
+
+            foreach (Task l in list)
+            {
+
+                l.PrintInfo();
+            }
+            return list;
+
+        }
+        public static List<Task> StatusIs(string input, int number, List<Task> statusUpdate)
+        {
+            string userSaid = input.ToLower();
+            if (userSaid == "yes" || userSaid == "y")
+            {
+                if (number == 0)
+                {
+                    statusUpdate[0].Status = "[X] Complete";
+                    return statusUpdate;
+                }
+                if (number == 1)
+                {
+                    statusUpdate[1].Status = "[X] Complete";
+                    return statusUpdate;
+                }
+                if (number == 2)
+                {
+                    statusUpdate[2].Status = "[X] Complete";
+                    return statusUpdate;
+                }
+                if (number == 3)
+                {
+                    statusUpdate[3].Status = "[X] Complete";
+                    return statusUpdate;
+                }
+                else
+                {
+                    return statusUpdate;
+                }
+            }
+            else
+            {
+                return statusUpdate;
+            }
+        }
+        //public static DateTime DateReader(string prompt)
+        //{
+
+        //    string dateSaid = Console.ReadLine();
+        //    string[] dateStrings = { dateSaid };
+
+
+        //    foreach (string dateString in dateStrings)
+        //    {
+        //        DateTime convertedDate = DateTime.Parse(dateString);
+        //        //string dateIs = string.Parse(convertedDate);
+        //       return  Console.WriteLine($"{convertedDate}");
+        //        convertedDate;
+        //    }
+        //}
+
+
+
+    }
+}
